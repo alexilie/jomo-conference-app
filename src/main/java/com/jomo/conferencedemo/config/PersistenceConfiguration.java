05814d407c5e620df8ee82612fc01480027a7281
alexilie <sorin.alex.ilie@gmail.com>
2022-10-05 15:05:09 -0400

    Update PersistenceConfiguration.java

--- src/main/java/com/jomo/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/jomo/conferencedemo/config/PersistenceConfiguration.java
@@ -21,11 +21,11 @@ public class PersistenceConfiguration {
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         // TODO - remove this code as not secure
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/conference-demo");
-        dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("alex1900");
+        dataSourceBuilder.url("jdbc:postgresql://ec2-107-23-76-12.compute-1.amazonaws.com:5432/dai0lecjnptsjb");
+        dataSourceBuilder.username("vicqkvnveokqnf");
+        dataSourceBuilder.password("d4ca9b2386d0032f56f947413db9d1fd255c54745977b3dd12f3f62868015c94");
 
-        System.out.println("******* PersistenceConfiguration Datasource has been properly initializer and set*********");
+        System.out.println("******* Heroku PostgreSQL instance - PersistenceConfiguration Datasource has been properly initializer and set*********");
         return dataSourceBuilder.build();
     }
 }
